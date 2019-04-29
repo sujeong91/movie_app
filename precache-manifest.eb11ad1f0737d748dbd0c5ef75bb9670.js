f29f894328435ba320756a1dc5a9cfc843c8884f
sujeong91 <susu.91@daum.net>
2019-04-29 16:24:55 +0900

    Updates

---
+++ precache-manifest.eb11ad1f0737d748dbd0c5ef75bb9670.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a85e9085d07dedd2c08e8e95c94828",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "6e883ae8eb3086f9e0ee",
+    "url": "/movie_app/static/css/main.21ff942e.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/movie_app/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "6e883ae8eb3086f9e0ee",
+    "url": "/movie_app/static/js/main.12afdf41.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
